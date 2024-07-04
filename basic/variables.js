50a1f3de40d15a5321d13542500bbc92433db5ae
AZIM BEG <118754891+azimbeg@users.noreply.github.com>
2024-07-04 15:23:11 +0000

    add new file

---
+++ basic/variables.js
@@ -0,0 +1,10 @@
+const accountId = 123456;
+let accountEmail = "azim@gmai.com";
+var accountPassword = "123456";
+/*
+    not to use var 
+    because of issue in block scope and functional scope
+*/
+accountCity = "Jaipur";
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
